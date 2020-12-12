f33673509590455e1a2d9ed9f6073b603a1902b3
Nick Piepmeier <pieps@google.com>
2020-12-12 20:19:32 +0000

    Day 5-2.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let f = File::open("data.txt")?;
     let reader = BufReader::new(f);
     let lines = reader.lines().flatten();
-    let max: u32 = lines.map(day5_1::parse_pass).max().unwrap();
-    println!("{}", max);
+    let used_seats: Vec<u32> = lines.map(day5_1::parse_pass).collect();
+    println!("{}", day5_1::find_needle(&used_seats));
     Ok(())
 }
