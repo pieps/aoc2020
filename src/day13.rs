9756cd3cd900e6077c90da6d2d9cbe0ef0280ab4
pieps <nick@pieps.dev>
2022-02-13 12:58:03 -0600

    Attempted to find the correct time by a modified LCM approach.
    The approach in https://en.wikipedia.org/wiki/Least_common_multiple#Using_a_simple_algorithm
    gives a way to find the LCM of a _set_ of numbers, which is useful here.
    
    It's possible to subtract the time offsets from each of the buses at the
    start, so instead of looking for T such that
    `T % b1 == 0 && (T+1) % b2 == 0 && ...`
    
    Instead we're looking for `T == bootleg_lcm([b1, b2-1, b3-3, ...], [b1,
    b2, b3, ...])`, incrementing beginning numbers by the bus number.
    
    This worked great for the example sets! But it's... not super fast. I
    let it run on my i7-1165G7 for several minutes, and while it _did_ spend
    about 10% of my battery, it did not find a solution.
    
    Back to the drawing board. I was going to try a Euclidean Algorithm
    approach in hopes that that'd perform a bit better (see code), but I was
    having a hell of a time figuring out how to factor the offsets in.
    
    Then I cheated and got a hint from Reddit. So I'm going to do that.

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 use crate::Day;
@@ -20,12 +22,13 @@ mod tests {
     fn day13_2_sample() {
         let lines = crate::split_input(SAMPLE);
         let d = Day13::new(lines);
+        assert_eq!(1068781, d.solve2().unwrap());
     }
 }
 
 pub struct Day13 {
     start: u32,
-    buses: Vec<u32>,
+    buses: HashMap<usize, u32>,
 }
 
 impl Day13 {
@@ -35,20 +38,72 @@ impl Day13 {
             .get(1)
             .unwrap()
             .split(',')
-            .filter(|b| *b != "x")
-            .map(str::parse)
-            .map(Result::unwrap)
-            .sorted()
+            .enumerate()
+            .filter(|(_, b)| *b != "x")
+            .map(|(a, b)| (a, b.parse().unwrap()))
             .collect();
+
         Box::new(Day13 { start, buses })
     }
+
+    fn modified_lcm(&self) -> i64 {
+        let orig: Vec<u32> = self.buses.values().copied().collect();
+        let mut sequence: Vec<i64> = self
+            .buses
+            .iter()
+            .map(|(k, v)| *v as i64 - *k as i64)
+            .collect();
+        loop {
+            if sequence.iter().unique().count() == 1 {
+                return *sequence.first().unwrap();
+            }
+            let lowest = sequence
+                .iter()
+                .enumerate()
+                .reduce(|(i, x), (j, y)| if x < y { (i, x) } else { (j, y) })
+                .unwrap()
+                .0;
+            let to_increment = sequence.get_mut(lowest).unwrap();
+            *to_increment = *to_increment + *orig.get(lowest).unwrap() as i64;
+        }
+    }
+}
+
+fn modified_euclidean(mut a: u32, mut b: u32, a_offset: u32, b_offset: u32) -> u32 {
+    a = a - a_offset;
+    b = b - b_offset;
+    if a < b {
+        euclidean(b, a);
+    }
+    while b != 0 {
+        let temp = b;
+        b = a.rem_euclid(b);
+        a = temp;
+    }
+    a + a_offset
+}
+
+fn modified_lcm(a_step: u32, b_step: u32, a_offset: u32, b_offset: u32) -> u32 {
+    (a_step + a_offset) * (b_step + b_offset) / euclidean(a_step, b_step)
+}
+
+fn euclidean(mut a: u32, mut b: u32) -> u32 {
+    if a < b {
+        euclidean(b, a);
+    }
+    while b != 0 {
+        let temp = b;
+        b = a.rem_euclid(b);
+        a = temp;
+    }
+    a
 }
 
 impl Day for Day13 {
     fn solve1(&self) -> Result<i64, Box<dyn std::error::Error>> {
-        let mut soonest = *self.buses.first().unwrap();
-        for &bus in &self.buses {
-            if self.start.rem_euclid(bus) > self.start.rem_euclid(soonest) {
+        let mut soonest = *self.buses.values().next().unwrap();
+        for &bus in self.buses.values() {
+            if bus - self.start.rem_euclid(bus) < soonest - self.start.rem_euclid(soonest) {
                 soonest = bus;
             }
         }
@@ -56,6 +111,11 @@ impl Day for Day13 {
     }
 
     fn solve2(&self) -> Result<i64, Box<dyn std::error::Error>> {
-        todo!()
+        let sequence = self
+            .buses
+            .iter()
+            .reduce(|acc, elem| modified_lcm(*acc.1, *elem.1, *acc.0 as u32, *elem.0 as u32))
+            .unwrap();
+        Ok(self.modified_lcm() as i64)
     }
 }
